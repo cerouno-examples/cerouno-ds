4c2341b919d336aa647a6e03d77546711f87a02a
Israel Zúñiga de la Mora <israelzdlm@gmail.com>
2018-05-12 14:01:16 -0500

    modified: TimeDeltaSensor:  dags/israel_02.py

--- 2_Python-Cientifico_Ingenieria-de-Datos/3_docker/dags/israel_02.py
+++ 2_Python-Cientifico_Ingenieria-de-Datos/3_docker/dags/israel_02.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.sensors import TimeSensor
+from airflow.operators.sensors import TimeDeltaSensor
 from airflow.operators.python_operator import PythonOperator
 
 
@@ -18,7 +18,7 @@ default_args = {
 }
 
 
-with DAG('airflow_cerouno_v02',
+with DAG('airflow_cerouno_v03',
          default_args=default_args,
          schedule_interval='0 * * * *',
          ) as dag:
@@ -27,8 +27,8 @@ with DAG('airflow_cerouno_v02',
                                bash_command='echo "hello"')
     # sleep = BashOperator(task_id='sleep',
     #                      bash_command='sleep 5')
-    sensor_dormilon = TimeSensor(task_id='sensor_dormilon',
-                                target_time=dt.time(5))
+    sensor_dormilon = TimeDeltaSensor(task_id='sensor_dormilon',
+                                delta=dt.timedelta(minutes=5))
     print_world = PythonOperator(task_id='print_world',
                                  python_callable=print_world)
 
